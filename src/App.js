b360a1f193382873a8524bc2cc3ccb16f8927e9b
Pratik Sharma <sharma.pratik2016@gmail.com>
2022-01-28 18:44:38 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useRef } from 'react';
+import Crossword from '@jaredreisinger/react-crossword';
+import { data } from './data';
+import { Toaster, toast } from 'react-hot-toast';
+
+import useSound from 'use-sound';
+
+import CorrectSound from './sounds/correct.wav';
+import Audience from './sounds/audience.wav'
+import Confetti from 'react-confetti';
+import useWindowSize from 'react-use/lib/useWindowSize';
+
+import { AiFillPlayCircle} from 'react-icons/ai'
+
+
+
 
 function App() {
+  const [playbackRate, setPlaybackRate] = React.useState(1);
+
+  const [win , setWin] = React.useState(false);
+
+  const { width, height } = useWindowSize()
+
+  const [open , setOpen] = React.useState(false);
+
+  const htmlRef = useRef(null);
+
+  const [play] = useSound(CorrectSound, {
+    playbackRate,
+    interrupt: true,
+  });
+
+  const [congratulate, {stop}] = useSound(Audience)
+
+  const playAgain = () => {
+    htmlRef.current.reset();
+    setWin(false);
+    stop();
+  }
+  const fillallanswer = () => {
+    htmlRef.current.fillAllAnswers();
+  }
+
+
+  const onWinning = (value) => {
+    const modal = () => {
+      return (
+        <div style={{zIndex:'1000',boxShadow:'' }}>
+        <h3>
+       CONGRATULATIONS!!!!! YOU WON 
+     </h3>
+     <button onClick={playAgain}>Play Again <AiFillPlayCircle/> </button>
+
+     </div>
+      )
+    }
+
+    if(value === true) {
+       modal(true)
+      return  setWin(true);
+
+    }
+    
+  
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{maxWidth: '500px', margin:'5%'}}>
+      <Confetti
+      width={width}
+      height={height}
+      run={win}
+      tweenDuration={3000}
+      onCanPlay={false}
+      
+    />
+      <Toaster
+      position="top-center"
+      reverseOrder={false}
+    />  
+  
+      <Crossword
+      ref={htmlRef}
+      data={data}
+
+      onCrosswordCorrect={onWinning}
+      onLoadedCorrect={() => {setWin(false)}}
+      onCorrect={(number, direction, answer) => {console.log(answer, direction); play(); toast.success(`you succesfully answered: ${answer}`)   } }/>
+
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,235 @@
+export const data = {
+    across: {
+      1: {
+        clue: 'a type of drug found in many over-the-counter and prescription medicines. Aspirin is the most common type of.(plural) (11) ',
+        answer: 'SALICYLATES',
+        row: 0,
+        col: 0,
+      },
+      2: {
+        clue: 'a Protein Coding gene. Diseases associated include Peroneal Nerve Paralysis.(acronym) (3)',
+        answer: 'IPP', 
+        row: 0, 
+        col: 12,
+      },
+      8: {
+          clue: 'an extracellular enzyme on the vascular endothelial surface that degrades circulating triglycerides in the bloodstream.(shortform) 3', 
+          answer: 'LPL',
+          row: 14, 
+          col: 0
+      },
+      9: {
+          clue: 'medical name for the feeling of extreme thirstiness', 
+          answer:'POLYDIPSIA', 
+          row:14 , 
+          col: 4
+      },
+      11: {
+        clue: 'a bicyclic heterocycle 3,7-dihydropurin-6-one that is an intermediate in the biosynthesis of uric acid(12)', 
+        answer: 'HYPOXANTHINE', 
+        row: 6,
+        col: 1
+      },
+      14: {
+          clue: 'an organ which give visuals to the brain(3)', 
+          answer: 'EYE', 
+          row: 12, 
+          col: 4
+      },
+      16: {
+          clue: 'one of a pair of diastereomers(6)', 
+          answer: 'EPIMER', 
+          row: 13, 
+          col:2
+      },
+      19: {
+          clue: 'a vision condition in which the eyes make repetitive, uncontrolled movements(9)', 
+          answer: 'NYSTAGMUS', 
+          row: 7, 
+          col: 4
+      }, 
+      20: {
+          clue: 'a multiprotein complex that incorporates one strand of a small interfering RNA (siRNA) or micro RNA (miRNA). (shortform) (4)', 
+          answer: 'RISC', 
+          row: 2,
+          col: 0
+      }
+    },
+    down: {
+      1: {
+        clue: 'non-essential amino acid that can be either taken up by the cell or synthesized de novo from glycolytic intermediates(6)',
+        answer: 'SERINE',
+        row: 0,
+        col: 0,
+      },
+      3: {
+          clue: 'Acid that is the precursors to proteins', 
+          answer: 'AMINO', 
+          row: 0,
+          col: 1
+      },
+      4: {
+          clue: 'a pre-mRNA element which regulates splicing acting in cis.(shortform) (3)', 
+          answer:'SRE', 
+          row: 2, 
+          col: 2,
+      },
+      5: {
+          clue: 'observed in bacteria with “spacer” DNA sequences in between the repeats that exactly match viral sequences and may be future of gene editing (shortform) (6)', 
+          answer: "CRISPR", 
+          row: 2,
+          col: 3
+      }, 
+      6: {
+          clue: 'large, double-ring oligomeric proteins that act as containers for the folding of other protein subunits.(plural).(4)', 
+          answer: 'CHAPERONINS', 
+          row: 0, 
+          col: 4
+      },
+      38: {
+        clue:'a type of RNA molecule that helps decode a messenger RNA (mRNA) sequence into a protein',
+        answer: 'TRNA',
+        row: 9,
+        col: 3
+      },
+      7: {
+          clue: 'single-cell organism, called Saccharomyces cerevisiae, which needs food, warmth, and moisture to thrive', 
+          answer: 'YEAST',
+          row: 0, 
+          col: 5,
+      }, 
+      10: {
+          clue: 'a protein naturally found in some grains including wheat, barley, and rye. It acts like a binder, holding food together and adding a “stretchy” quality.(6)', 
+          answer:'GLUTEN', 
+          row: 0, 
+          col: 14
+      },
+      12: {
+        clue: 'gut-derived peptide hormones that are rapidly secreted in response to a meal. (8)', 
+        answer: 'INCRETIN',
+        row: 6,
+        col: 14 
+      },
+      13: {
+          clue: 'When separated from the rest of the blood, it is a light yellow liquid. (6)',
+          answer: 'PLASMA', 
+          row: 0,
+          col: 13
+      }, 
+      15: {
+          clue: 'a symptom of gout, a condition where uric acid crystallizes in joints like those in your feet and hands.(5)', 
+          answer:'TOPHI',
+          row: 0,
+          col: 8
+      },
+      17: {
+          clue: 'remove (an adsorbed substance) by washing with a solvent, especially in chromatography.(5)', 
+          answer: 'ELUTE', 
+          row: 0, 
+          col: 9
+      }, 
+      18: {
+          clue: 'a jelly-like substance consisting of polysaccharides obtained from the cell walls of some species of red algae.(4)', 
+          answer: 'AGAR', 
+          row: 1, 
+          col: 11
+      },
+      21: {
+          clue: 'ringed lipids that play a role in the membrane integrity of eukaryotes', 
+          answer: 'STEROL', 
+          row: 9, 
+          col: 0
+      },
+      22: {
+          clue: 'anticoagulant (blood thinner) that prevents the formation of blood clots', 
+          answer: 'HEPARIN', 
+          row: 6, 
+          col: 1
+      },
+      23: {
+          clue: 'an enzyme that catalyzes the transfer of phosphate groups from high-energy, phosphate-donating molecules to specific substrates', 
+          answer: 'KINASE', 
+          row: 8,
+          col: 2
+      }, 
+      24: {
+          clue: 'an enzyme that catalyses the hydrolysis of starch into sugars',  
+          answer: 'AMYLASE',
+          row: 1, 
+          col: 6
+      },
+      25: {
+          clue: 'wander aimlessly. not rellated to biology. (6)', 
+          answer:'STREEL', 
+          row: 9,
+          col: 6
+      },
+      26: {
+          clue: 'a toxin or other foreign substance which induces an immune response in the body, especially the production of antibodies.', 
+          answer:'ANTIGEN', 
+          row: 5, 
+          col: 7
+      },
+      27: {
+          clue: 'a sequence of DNA found in the core promoter region of genes in archaea and eukaryotes', 
+          answer:'TATABOX', 
+          row: 6, 
+          col: 8
+      }, 
+      28: {
+          clue: 'ek bahut sundar sundar name. ', 
+          answer:'OJAL', 
+          row:9,
+          col: 9
+      }, 
+      29: {
+          clue: 'gene, a DNA-binding transcription factor that represses transcription of the operon involved in transport and catabolism of lactose', 
+          answer:'LACI', 
+          row: 3, 
+          col: 10
+      }, 
+      30: {
+          clue: 'either of the two main categories (male and female) into which humans and most other living things are divided on the basis of their reproductive functions.', 
+          answer:'SEX', 
+          row: 0, 
+          col: 10
+      }, 
+      31: {
+          clue: "looks pretty, in its shiny, fast-moving liquid form, but don't touch! It can be extremely poisonous to humans. ", 
+          answer:'MERCURY', 
+          row: 7, 
+          col: 10
+      },
+      32: {
+          clue: 'a protein, sensors and mediators that regulate cholesterol homeostasis through binding to SCAP and HMG-CoA reductase in diverse tissues such as adipose tissue and liver, as well as the cultured cells', 
+          answer: 'INSIG', 
+          row: 0, 
+          col: 12
+      }, 
+      33: {
+          clue: 'a hormone, part of menstrual cycle, helps you get pregnant, and plays a role in helping you develop bones and grow hair. It also helps regulate your moods and impacts your brain development and structure', 
+          answer: "ESTROGEN", 
+          row: 6, 
+          col: 12 
+      },
+      34: {
+          clue: 'an iron-containing porphyrin with chlorine that can be formed from a heme group, such as heme B found in the hemoglobin of human blood', 
+          answer: 'HEMIN', 
+          row: 8, 
+          col: 13
+      }, 
+      35: {
+          clue: 'the first cell organelle discovered',
+          answer:'NUCLEUS', 
+          row: 6, 
+          col: 11
+      }, 
+      36: {
+          clue:'neurotransmitter that blocks impulses between nerve cells in the brain.(shortform). (4)',
+          answer:'GABA',
+          row: 9,
+          col: 5
+      }
+
+    },
+  }
